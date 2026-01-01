dbe71c9f6ea522d9312c69e33d29a50966ee4ade
Rubel224603 <cserubelhosen@gmail.com>
2026-01-01 19:35:43 +0600

    db added

---
+++ database/migrations/2026_01_01_131004_create_course_modules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_modules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id')->nullable();
+            $table->string('_id')->nullable();
+            $table->string('topic')->nullable();
+            $table->string('no')->nullable();
+            $table->string('mode')->nullable();
+            $table->string('t_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_modules');
+    }
+};
